b1aad9aa93f11a82cb8a538bcb3c10b0e4dbd920
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-07-01 15:42:18 +0300

    adds the function pizza to declare variables for use in the order manipulation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function Pizza(size, toppings, crust, total, orderNo) {
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust
+    this.total = total;
+    this.orderNo = orderNo;
+
+
+}
