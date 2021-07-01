b65519be5a8d436d94acb3083039ebd68f89327a
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-07-01 15:44:06 +0300

    adds an order function tat calulcates the ordr amount

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ function Pizza(size, toppings, crust, total, orderNo) {
 
 
 }
+$('#order').click(function() {
+    var sizeOfPizza = $(".size option:selected").val();
+    var toppingsOfPizza = $(".toppings option:selected").val();
+    var crustOfPizza = $(".crust option:selected").val();
+    var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+    var order = 1;
+    var grandTotal = 0;
+
+    let totalpizza = [];
+    grandTotal = total + pizza;
+    $.each($("input[name='pizza']:selected"), function() {
+        pizzaselected.push($(this).val());
+        $("#add-pizza").append(newRow);
+    });
+
+
+
+})
